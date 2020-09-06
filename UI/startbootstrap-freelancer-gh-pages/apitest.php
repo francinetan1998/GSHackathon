71650173cae013294f01f6d0feda1200542e91bc
francinetan.2018 <francinetan.2018@sis.smu.edu.sg>
2020-09-06 13:39:30 +0800

    Create API template

---
+++ UI/startbootstrap-freelancer-gh-pages/apitest.php
@@ -0,0 +1,46 @@
+<?php
+
+function callAPI($method, $url, $data){
+   $curl = curl_init();
+   switch ($method){
+      case "POST":
+         curl_setopt($curl, CURLOPT_POST, 1);
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         break;
+      case "PUT":
+         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+         break;
+      default:
+         if ($data)
+            $url = sprintf("%s?%s", $url, http_build_query($data));
+   }
+
+   // OPTIONS:
+   curl_setopt($curl, CURLOPT_URL, $url);
+   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+   ));
+
+   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+   // EXECUTE:
+   $result = curl_exec($curl);
+   if(!$result){ die("Connection Failure"); }
+   curl_close($curl);
+   return $result;
+}
+
+$data = array(
+    'q' => 'bitcoin',
+    'from' => '2020-08-06',
+    'sortBy' => 'publishedAt',
+    'apiKey' => 'daa1f687c4e149de9f5a46ab6604003c'
+);
+
+echo callAPI("GET", "http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-06&sortBy=published", $data);
+
+?>
